2e86bf0a87d781b66bd9e695edfa3612a7ef952d
rachelxie <rachelxie1985@gmail.com>
2017-06-12 17:31:22 +1200

    Establish the new Console application with prompt and reading in the paragraph.

---
+++ ConsoleWorldCount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWorldCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a paragraph here (no numbers): ");
+            var para = Console.ReadLine();
+            Console.WriteLine("{0}", para);
+            Console.ReadKey();
+        }
+    }
+}
